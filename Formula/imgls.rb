acb31fd28f852bdcd5ffcb0b9321c20f4799c24f
Daniel Solano Gómez <daniel@solanogomez.org>
2017-03-23 16:02:00 -0500

    Fix imgls formula

--- Formula/imgls.rb
+++ Formula/imgls.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Imgcat < Formula
+class Imgls < Formula
   homepage 'https://www.iterm2.com/images.html'
   url 'https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgls'
   sha256 '180d248be0d82f617eb513138448389b6898a6227c09dc0be987ae1819e201f2'
